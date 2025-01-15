77d1e2809eb15ce69707169b4660006b1e5e0dfd
Guilherme Mattoso <85652034+mattosoguilherme@users.noreply.github.com>
2025-01-15 01:21:57 -0300

    atualização

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,59 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty({
+    default: 'teste@gmail.com',
+  })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: 'Teste da Silva',
+  })
+  nome: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: '119760706070',
+  })
+  contato: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: 'Teste',
+  })
+  produto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: 'Mogi 2',
+  })
+  unidade: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: '357662',
+  })
+  matricula: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: '2021-10-10',
+  })
+  aniversario: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/config/prisma.client';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+constructor( private prisma: PrismaService) {}
+
+  create(createUserDto: CreateUserDto):Promise<User> {
+
+    
+    return ;
   }
 
   findAll() {
