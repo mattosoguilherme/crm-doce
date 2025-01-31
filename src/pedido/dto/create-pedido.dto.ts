4c8b52fffa3441b20e27bfe81b634473483100dd
Guilherme Mattoso <85652034+mattosoguilherme@users.noreply.github.com>
2025-01-31 18:16:24 -0300

    ..

--- src/pedido/dto/create-pedido.dto.ts
+++ src/pedido/dto/create-pedido.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePedidoDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePedidoDto {
+  @IsString({ message: 'O campo user_id deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo user_id não pode ser vazio' })
+  @ApiProperty( { default: 'cm6e0rumn0000pzf89ldfix8m' } )
+  user_id: string;
+
+  @IsArray({ message: 'O campo items_id deve ser um array' })
+  @IsNotEmpty({ message: 'O campo items_id não pode ser vazio' })
+  @ApiProperty( { default: [1] } )
+  items_id: number[];
+
+  @IsString({ message: 'O campo status deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo status não pode ser vazio' })
+  @ApiProperty( { default: 'PENDENTE' } )
+  status: string;
+
+}
--- src/pedido/dto/update-pedido.dto.ts
+++ src/pedido/dto/update-pedido.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreatePedidoDto } from './create-pedido.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdatePedidoDto extends PartialType(CreatePedidoDto) {}
+export class UpdatePedidoDto extends PartialType(CreatePedidoDto) {
+  @IsOptional()
+  items_id?: number[];
+
+  @IsOptional()
+  status?: string;
+
+  @IsOptional()
+  user_id?: string;
+}
