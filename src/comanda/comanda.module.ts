4483301ca4c20eb5259edc94e94496299973507b
Guilherme Mattoso <85652034+mattosoguilherme@users.noreply.github.com>
2025-05-04 17:03:44 -0300

    por hora finalizado

--- src/comanda/comanda.module.ts
+++ src/comanda/comanda.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ComandaService } from './comanda.service';
 import { ComandaController } from './comanda.controller';
+import { PrismaService } from 'src/config/prisma.client';
+import { CrmService } from 'src/crm.service';
+import { UserService } from 'src/user/user.service';
+import { PedidoService } from 'src/pedido/pedido.service';
 
 @Module({
   controllers: [ComandaController],
-  providers: [ComandaService],
+  providers: [
+    ComandaService,
+    PrismaService,
+    CrmService,
+    UserService,
+    PedidoService,
+  ],
 })
 export class ComandaModule {}
--- src/comanda/comanda.service.ts
+++ src/comanda/comanda.service.ts
@@ -1,11 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateComandaDto } from './dto/create-comanda.dto';
 import { UpdateComandaDto } from './dto/update-comanda.dto';
+import { PrismaService } from 'src/config/prisma.client';
+import { CrmService } from 'src/crm.service';
+import { UserService } from 'src/user/user.service';
+import { PedidoService } from 'src/pedido/pedido.service';
 
 @Injectable()
 export class ComandaService {
-  create(createComandaDto: CreateComandaDto) {
-    return 'This action adds a new comanda';
+  constructor(
+    private prisma: PrismaService,
+    private crm: CrmService,
+    private userService: UserService,
+    private pedidoService: PedidoService,
+  ) {}
+
+  async create(createComandaDto: CreateComandaDto) {
+    const users = await this.userService.findAll();
+    const pedidos = await this.pedidoService.findAll();
+    const createdComandas = [];
+
+    for (const user of users) {
+      let total = 0;
+      const comanda = pedidos.filter((p) => p.userId === user.id);
+      for (const c of comanda) {
+        total += c.total;
+      }
+
+     const createComanda = await this.prisma.comanda.create({
+        data: {
+          nome_cliente: user.nome,
+          total: total,
+          saldo_quitado: 0,
+          saldo_pendente: 0,
+          status: 'PENDENTE',
+          Pedidos: { connect: comanda.map((c) => ({ id: c.id })) },
+          user: { connect: { id: user.id } },
+        },
+      });
+
+      createdComandas.push(createComanda)
+    }
+
+    return createdComandas;
   }
 
   findAll() {
